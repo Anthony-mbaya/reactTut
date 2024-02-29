fe2848174e37b410109d70f2aab8fc42f8bda9f8
Anthony-Mbaya <120335962+Anthony-mbaya@users.noreply.github.com>
2024-02-29 13:04:20 +0300

    Added my files in the github using git bush

---
+++ react.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const getRoot = document.getElementById("linkRoot");
+const root = ReactDOM.createRoot(getRoot);
+root.render(<>HELLO IS REACT DEV TONNY</>)
